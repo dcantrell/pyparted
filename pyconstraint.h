4e4c238352f34189d052c0754839a1f3f1c4bd7c
David Cantrell <dcantrel@mortise.boston.redhat.com>
2007-08-10 16:22:15 -0400

    Relicense pyparted as GPL version 2 or any later version.

--- pyconstraint.h
+++ pyconstraint.h
@@ -1,17 +1,25 @@
-/* -*- Mode: c; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 8 -*-
- * Matt Wilson <msw@redhat.com>
+/*
+ * pyconstraint.h
  *
- * Copyright 2000 Red Hat, Inc.
+ * Copyright (C) 2000  Red Hat, Inc.  All rights reserved.
  *
- * This software may be freely redistributed under the terms of the GNU
- * library public license.
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions of
+ * the GNU General Public License v.2, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY expressed or implied, including the implied warranties of
+ * MERCHANTABILITY or FITNESS FOR A * PARTICULAR PURPOSE.  See the GNU General
+ * Public License for more details.  You should have received a copy of the
+ * GNU General Public License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.  Any Red Hat trademarks that are incorporated in the
+ * source code or documentation are not subject to the GNU General Public
+ * License and may only be used or replicated with the express permission of
+ * Red Hat, Inc.
  *
- * You should have received a copy of the GNU Library Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
- *
-*/
+ * Red Hat Author(s): Matt Wilson
+ *                    David Cantrell
+ */
 
 #ifndef PYCONSTRAINT_H_INCLUDED
 #define PYCONSTRAINT_H_INCLUDED
