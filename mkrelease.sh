404eca7d2c61d3b71e69bccef7baa241b0f55bb9
David Cantrell <dcantrell@redhat.com>
2007-10-01 14:40:00 -0400

    Beginnings of the autoconf/automake transition for the source code.

---
+++ mkrelease.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+if [ -f Makefile ]; then
+    make -k distclean
+fi
+aclocal -I m4
+#libtoolize --copy --force
+autoconf
+autoheader
+touch config.h.in
+automake --foreign --add-missing --copy
+rm -rf autom4te.cache
