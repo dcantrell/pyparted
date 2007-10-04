f90a34efc5fd14939e8a3ff34612ea20c525b0c8
David Cantrell <dcantrel@mortise.boston.redhat.com>
2007-10-04 13:23:02 -0400

    Added pyunit.h and pyunit.c, which are the Python 1:1 function mappings for libparted/unit.c.

---
+++ include/pyunit.h
@@ -0,0 +1,109 @@
+/*
+ * pyunit.h
+ * pyparted type definitions for pyunit.c
+ *
+ * Copyright (C) 2007  Red Hat, Inc.
+ * All rights reserved.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions of
+ * the GNU General Public License v.2, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY expressed or implied, including the implied warranties of
+ * MERCHANTABILITY or FITNESS FOR A * PARTICULAR PURPOSE.  See the GNU General
+ * Public License for more details.  You should have received a copy of the
+ * GNU General Public License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.  Any Red Hat trademarks that are incorporated in the
+ * source code or documentation are not subject to the GNU General Public
+ * License and may only be used or replicated with the express permission of
+ * Red Hat, Inc.
+ *
+ * Red Hat Author(s): David Cantrell
+ */
+
+#ifndef PYUNIT_H_INCLUDED
+#define PYUNIT_H_INCLUDED
+
+#include <Python.h>
+#include <structmember.h>
+
+#include <parted/parted.h>
+
+/* _ped.Unit type is the Python equivalent of PedUnit in libparted */
+typedef struct {
+    PyObject_HEAD
+
+    /* a PedUnit is an enumerated type, so we'll just store a long here */
+    long val;
+} _ped_Unit;
+
+static PyMemberDef _ped_Unit_members[] = {
+    {"val", T_LONG, offsetof(_ped_Unit, val), 0, "PedUnit value"},
+    {NULL}
+};
+
+static PyMethodDef _ped_Unit_methods[] = {
+    {NULL}
+};
+
+void _ped_Unit_dealloc(_ped_Unit *self);
+PyObject *_ped_Unit_new(PyTypeObject *type, PyObject *args, PyObject *kwds);
+int _ped_Unit_init(_ped_Unit *self, PyObject *args, PyObject *kwds);
+
+static PyTypeObject _ped_UnitType = {
+    PyObject_HEAD_INIT(NULL)
+    0,                                         /* ob_size */
+    "_ped.Unit",                               /* tp_name */
+    sizeof(_ped_Unit),                         /* tp_basicsize */
+    0,                                         /* tp_itemsize */
+    (destructor) _ped_Unit_dealloc,            /* tp_dealloc */
+    0,                                         /* tp_print */
+    0,                                         /* tp_getattr */
+    0,                                         /* tp_setattr */
+    0,                                         /* tp_compare */
+    0,                                         /* tp_repr */
+    0,                                         /* tp_as_number */
+    0,                                         /* tp_as_sequence */
+    0,                                         /* tp_as_mapping */
+    0,                                         /* tp_hash */
+    0,                                         /* tp_call */
+    0,                                         /* tp_str */
+    0,                                         /* tp_getattro */
+    0,                                         /* tp_setattro */
+    0,                                         /* tp_as_buffer */
+    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,  /* tp_flags */
+    "PedUnit objects",                         /* tp_doc */
+    0,                                         /* tp_traverse */
+    0,                                         /* tp_clear */
+    0,                                         /* tp_richcompare */
+    0,                                         /* tp_weaklistoffset */
+    0,                                         /* tp_iter */
+    0,                                         /* tp_iternext */
+    _ped_Unit_methods,                         /* tp_methods */
+    _ped_Unit_members,                         /* tp_members */
+    0,                                         /* tp_getset */
+    0,                                         /* tp_base */
+    0,                                         /* tp_dict */
+    0,                                         /* tp_descr_get */
+    0,                                         /* tp_descr_set */
+    0,                                         /* tp_dictoffset */
+    (initproc) _ped_Unit_init,                 /* tp_init */
+    0,                                         /* tp_alloc */
+    _ped_Unit_new,                             /* tp_new */
+};
+
+/* 1:1 function mappings for unit.h in libparted */
+PyObject *py_ped_unit_set_default(PyObject *s, PyObject *args);
+PyObject *py_ped_unit_get_default(PyObject *s, PyObject *args);
+PyObject *py_ped_unit_get_size(PyObject *s, PyObject *args);
+PyObject *py_ped_unit_get_name(PyObject *s, PyObject *args);
+PyObject *py_ped_unit_get_by_name(PyObject *s, PyObject *args);
+PyObject *py_ped_unit_format_custom_byte(PyObject *s, PyObject *args);
+PyObject *py_ped_unit_format_byte(PyObject *s, PyObject *args);
+PyObject *py_ped_unit_format_custom(PyObject *s, PyObject *args);
+PyObject *py_ped_unit_format(PyObject *s, PyObject *args);
+PyObject *py_ped_unit_parse(PyObject *s, PyObject *args);
+PyObject *py_ped_unit_parse_custom(PyObject *s, PyObject *args);
+
+#endif /* PYUNIT_H_INCLUDED */
---
+++ src/pyunit.c
@@ -0,0 +1,133 @@
+/*
+ * pyunit.c
+ *
+ * Copyright (C) 2007  Red Hat, Inc.
+ * All rights reserved.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions of
+ * the GNU General Public License v.2, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY expressed or implied, including the implied warranties of
+ * MERCHANTABILITY or FITNESS FOR A * PARTICULAR PURPOSE.  See the GNU General
+ * Public License for more details.  You should have received a copy of the
+ * GNU General Public License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.  Any Red Hat trademarks that are incorporated in the
+ * source code or documentation are not subject to the GNU General Public
+ * License and may only be used or replicated with the express permission of
+ * Red Hat, Inc.
+ *
+ * Red Hat Author(s): David Cantrell
+ */
+
+#include <Python.h>
+
+#include "pyunit.h"
+
+/* _ped.Unit functions */
+void _ped_Unit_dealloc(_ped_Unit *self) {
+    self->ob_type->tp_free((PyObject *) self);
+}
+
+PyObject *_ped_Unit_new(PyTypeObject *type, PyObject *args, PyObject *kwds) {
+    _ped_Unit *self;
+
+    self = (_ped_Unit *) type->tp_alloc(type, 0);
+    return (PyObject *) self;
+}
+
+int _ped_Unit_init(_ped_Unit *self, PyObject *args, PyObject *kwds) {
+    long val;
+
+    if (PyArg_ParseTuple(args, "l", &val)) {
+        self->val = val;
+    }
+
+    return 0;
+}
+
+/* 1:1 function mappings for unit.h in libparted */
+PyObject *py_ped_unit_set_default(PyObject *s, PyObject *args) {
+    _ped_Unit *in;
+    PedUnit out;
+
+    if (!PyArg_ParseTuple(args, "O", (_ped_Unit *) &in)) {
+        /* FIXME - throw exception */
+        return NULL;
+    }
+
+    if (in->val < PED_UNIT_FIRST || in->val > PED_UNIT_TEBIBYTE) {
+        /* FIXME - throw range exception */
+        return NULL;
+    }
+
+    out = in->val;
+    ped_unit_set_default(out);
+
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+PyObject *py_ped_unit_get_default(PyObject *s, PyObject *args) {
+    PedUnit in = ped_unit_get_default();
+    _ped_Unit *out;
+
+    out = (_ped_Unit *) _ped_Unit_new(&_ped_UnitType, NULL, NULL);
+    out->val = in;
+    return (PyObject *) out;
+}
+
+PyObject *py_ped_unit_get_size(PyObject *s, PyObject *args) {
+    /* FIXME */
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+PyObject *py_ped_unit_get_name(PyObject *s, PyObject *args) {
+    /* FIXME */
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+PyObject *py_ped_unit_get_by_name(PyObject *s, PyObject *args) {
+    /* FIXME */
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+PyObject *py_ped_unit_format_custom_byte(PyObject *s, PyObject *args) {
+    /* FIXME */
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+PyObject *py_ped_unit_format_byte(PyObject *s, PyObject *args) {
+    /* FIXME */
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+PyObject *py_ped_unit_format_custom(PyObject *s, PyObject *args) {
+    /* FIXME */
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+PyObject *py_ped_unit_format(PyObject *s, PyObject *args) {
+    /* FIXME */
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+PyObject *py_ped_unit_parse(PyObject *s, PyObject *args) {
+    /* FIXME */
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+PyObject *py_ped_unit_parse_custom(PyObject *s, PyObject *args) {
+    /* FIXME */
+    Py_INCREF(Py_None);
+    return Py_None;
+}
