3bd42c271fb9de7d2d97bbfe223992586b85806a
David Cantrell <dcantrel@mortise.boston.redhat.com>
2007-10-09 00:56:45 -0400

    Implemented py_ped_constraint_init().

--- src/pyconstraint.c
+++ src/pyconstraint.c
@@ -23,7 +23,10 @@
 
 #include <Python.h>
 
+#include "convert.h"
 #include "pyconstraint.h"
+#include "pygeom.h"
+#include "pynatmath.h"
 
 /* _ped.Constraint functions */
 void _ped_Constraint_dealloc(_ped_Constraint *self) {
@@ -46,9 +49,42 @@ int _ped_Constraint_init(_ped_Constraint *self, PyObject *args,
 
 /* 1:1 function mappings for constraint.h in libparted */
 PyObject *py_ped_constraint_init(PyObject *s, PyObject *args) {
-    /* FIXME */
-    Py_INCREF(Py_None);
-    return Py_None;
+    _ped_Constraint *in_constraint;
+    _ped_Alignment *in_start_align, *in_end_align;
+    _ped_Geometry *in_start_range, *in_end_range;
+    _ped_Sector in_min_size, in_max_size;
+    PedConstraint *out_constraint;
+    PedAlignment *out_start_align, *out_end_align;
+    PedGeometry *out_start_range, *out_end_range;
+    PedSector out_min_size, out_max_size;
+
+    if (!PyArg_ParseTuple(args, "OOOOOOO", (_ped_Constraint *) &in_constraint,
+                                           (_ped_Alignment *) &in_start_align,
+                                           (_ped_Alignment *) &in_end_align,
+                                           (_ped_Geometry *) &in_start_range,
+                                           (_ped_Geometry *) &in_end_range,
+                                           (_ped_Sector *) &in_min_size,
+                                           (_ped_Sector *) &in_max_size)) {
+        return NULL;
+    }
+
+    out_constraint = _ped_Constraint2PedConstraint(in_constraint);
+    out_start_align = _ped_Alignment2PedAlignment(in_start_align);
+    out_end_align = _ped_Alignment2PedAlignment(in_end_align);
+    out_start_range = _ped_Geometry2PedGeometry(in_start_range);
+    out_end_range = _ped_Geometry2PedGeometry(in_end_range);
+    out_min_size = _ped_Sector2PedSector(in_min_size);
+    out_max_size = _ped_Sector2PedSector(in_max_size);
+
+    ret = ped_constraint_init(out_constraint, out_start_align, out_end_align,
+                              out_start_range, out_end_range,
+                              out_min_size, out_max_size);
+    free(out_constraint);
+    free(out_start_align);
+    free(out_end_align);
+    free(out_start_range);
+    free(out_end_range);
+    return Py_BuildValue("i", ret);
 }
 
 PyObject *py_ped_constraint_new(PyObject *s, PyObject *args) {
