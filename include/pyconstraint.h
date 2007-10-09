77e3e26d435a07dd362815bea0eb7e5a1f43e12a
David Cantrell <dcantrel@mortise.boston.redhat.com>
2007-10-09 11:21:06 -0400

    Note that start_range and end_range are _ped.Geometry() objects.

--- include/pyconstraint.h
+++ include/pyconstraint.h
@@ -37,8 +37,8 @@ typedef struct {
     /* PedConstraint members */
     PyObject *start_align;              /* _ped.Alignment */
     PyObject *end_align;                /* _ped.Alignment */
-    PyObject *start_range;              /* _ped.Alignment */
-    PyObject *end_range;                /* _ped.Alignment */
+    PyObject *start_range;              /* _ped.Geometry */
+    PyObject *end_range;                /* _ped.Geometry */
     PyObject *min_size;                 /* _ped.Sector */
     PyObject *max_size;                 /* _ped.Sector */
 } _ped_Constraint;
