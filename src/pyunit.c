761284de2aeb952233b37f124a84d10d382724e7
Chris Lumens <clumens@redhat.com>
2007-11-09 13:45:57 -0500

    Add error handling to the pyunit functions.

--- src/pyunit.c
+++ src/pyunit.c
@@ -9,7 +9,7 @@
  * the GNU General Public License v.2, or (at your option) any later version.
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY expressed or implied, including the implied warranties of
- * MERCHANTABILITY or FITNESS FOR A * PARTICULAR PURPOSE.  See the GNU General
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
  * Public License for more details.  You should have received a copy of the
  * GNU General Public License along with this program; if not, write to the
  * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
@@ -19,11 +19,13 @@
  * Red Hat, Inc.
  *
  * Red Hat Author(s): David Cantrell <dcantrell@redhat.com>
+ *                    Chris Lumens <clumens@redhat.com>
  */
 
 #include <Python.h>
 
 #include "convert.h"
+#include "exceptions.h"
 #include "pydevice.h"
 #include "pyunit.h"
 
@@ -36,7 +38,7 @@ PyObject *py_ped_unit_set_default(PyObject *s, PyObject *args) {
     }
 
     if (unit < PED_UNIT_FIRST || unit > PED_UNIT_LAST) {
-        /* XXX: some sort of range exception */
+        PyErr_SetString(PyExc_ValueError, "Invalid unit provided.");
         return NULL;
     }
 
@@ -65,6 +67,12 @@ PyObject *py_ped_unit_get_size(PyObject *s, PyObject *args) {
     }
 
     ret = ped_unit_get_size(out_dev, unit);
+    if (ret == 0) {
+        /* Re-raise the libparted exception. */
+        partedExnRaised = 0;
+        return NULL;
+    }
+
     ped_device_destroy(out_dev);
 
     return PyLong_FromLongLong(ret);
@@ -77,17 +85,29 @@ PyObject *py_ped_unit_get_name(PyObject *s, PyObject *args) {
         return NULL;
     }
 
+    if (unit < PED_UNIT_FIRST || unit > PED_UNIT_LAST) {
+        PyErr_SetString(PyExc_ValueError, "Invalid unit provided.");
+        return NULL;
+    }
+
     return Py_BuildValue("s", ped_unit_get_name(unit));
 }
 
 PyObject *py_ped_unit_get_by_name(PyObject *s, PyObject *args) {
+    PedUnit ret;
     char *name;
 
     if (!PyArg_ParseTuple(args, "s", &name)) {
         return NULL;
     }
 
-    return PyLong_FromLongLong(ped_unit_get_by_name(name));
+    ret = ped_unit_get_by_name(name);
+    if (ret == -1) {
+        PyErr_SetString(UnknownTypeException, name);
+        return NULL;
+    }
+
+    return PyLong_FromLongLong(ret);
 }
 
 PyObject *py_ped_unit_format_custom_byte(PyObject *s, PyObject *args) {
