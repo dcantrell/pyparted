31b5810c0f89161d0dd23fdceff5b3422a00737c
David Cantrell <dcantrell@redhat.com>
2008-11-18 12:44:18 -1000

    Add docstrings to include/pydevice.h

--- include/pydevice.h
+++ include/pydevice.h
@@ -48,6 +48,93 @@ PyObject *py_ped_device_sync_fast(PyObject *s, PyObject *args);
 PyObject *py_ped_device_check(PyObject *s, PyObject *args);
 PyObject *py_ped_device_get_constraint(PyObject *s, PyObject *args);
 
+PyDoc_STRVAR(device_get_next_doc,
+"get_next() -> Device\n\n"
+"Return the next Device in the list detected by _ped.device_probe_all().");
+
+PyDoc_STRVAR(device_is_busy_doc,
+"is_busy() -> bool\n\n"
+"Return True if this Device is currently in use, False otherwise.");
+
+PyDoc_STRVAR(device_open_doc,
+"open() -> bool\n\n"
+"Attempt to open this Device to allow use of read(), write(), and sync()\n"
+"methods.  The open() call is architecture-dependent.  Apart from\n"
+"requesting access to the device from the operating system, it does things\n"
+"flushing caches.\n\n"
+"This method may allocate internal resources depending on the architecture\n"
+"All allocated resources are freed when you call the close() method.\n\n"
+"Return True if the Device could be opened, False otherwise.");
+
+PyDoc_STRVAR(device_close_doc,
+"close() -> bool\n\n"
+"Close this Device.  All allocated resources are freed.  If a failure\n"
+"occurs while closing the Device, this method returns False.  The method\n"
+"returns True on success.");
+
+PyDoc_STRVAR(device_destroy_doc,
+"destroy() -> None\n\n"
+"Destroys the Device, removes it from the device list, destroys all\n"
+"allocated resources associated with it, and destroys the object.");
+
+PyDoc_STRVAR(device_cache_remove_doc,
+"cache_remove() -> None\n\n"
+"Remove the Device from the device list, but does not destroy it or any\n"
+"allocated resources associated with it.  USE WITH CAUTION.");
+
+PyDoc_STRVAR(device_begin_external_access_doc,
+"begin_external_access() -> bool"
+"Begins external access mode for this Device.  External access mode allows\n"
+"you to safely do I/O on the device.  If a Device is open, then you should\n"
+"not do any I/O on that Device, e.g. by calling an external program like\n"
+"e2fsck, unless you put it in external access mode.  You should not use\n"
+"any commands that do I/O to a Device while it is in external access mode.\n\n"
+"Also, you should not close a Device while it is in external access mode.\n\n"
+"Return True if the Device was successfully put in external access mode,\n"
+"False otherwise.");
+
+PyDoc_STRVAR(device_end_external_access_doc,
+"end_external_access() -> bool\n\n"
+"Ends external access mode for this Device.  Returns True on success,\n"
+"False on failure.");
+
+PyDoc_STRVAR(device_read_doc,
+"read(buffer, start, count) -> bool"
+"Read count sectors from this Device in to buffer, starting at sector start.\n"
+"Both start and count are long integers and buffer is a Python object large\n"
+"enough to hold what you want to read.\n\n"
+"Return True if the read was successful, False otherwise.");
+
+PyDoc_STRVAR(device_write_doc,
+"write(buffer, start, count) -> bool\n\n"
+"Write count sectors from buffer to this Device, starting at sector start.\n"
+"Both start and count are long integers and buffer is a Python object holding\n"
+"what you want to write to this Device.\n\n"
+"Return True if the write was successful, False otherwise.");
+
+PyDoc_STRVAR(device_sync_doc,
+"sync() -> bool\n\n"
+"Flushes all write-behind caches that might be holding up writes.  It is\n"
+"slow because it guarantees cache coherency among all relevant caches.\n"
+"Return True on success, False otherwise.");
+
+PyDoc_STRVAR(device_sync_fast_doc,
+"sync_fast() -> bool\n\n"
+"Flushes all write-behind caches that might be holding writes.  WARNING:\n"
+"Does NOT ensure cache coherency with other caches.  If you need cache\n"
+"coherency, use sync() instead.  Return True on success, False otherwise.");
+
+PyDoc_STRVAR(device_check_doc,
+"check() -> long int\n\n"
+"Architecture-dependent function that returns the number of sectors on\n"
+"this Device that are ok.");
+
+PyDoc_STRVAR(device_get_constraint_doc,
+"get_constraint() -> Constraint\n\n"
+"Get a Constraint that represents hardware requirements on alignment and\n"
+"geometry.  This is, for example, import for media that have a physical\n"
+"sector size that is a multiple of the logical sector size.");
+
 /* _ped.CHSGeometry type is the Python equiv of PedCHSGeometry in libparted */
 typedef struct {
     PyObject_HEAD
@@ -74,15 +161,23 @@ int _ped_CHSGeometry_set(_ped_CHSGeometry *self, PyObject *value,
 static PyGetSetDef _ped_CHSGeometry_getset[] = {
     {"cylinders", (getter) _ped_CHSGeometry_get,
                   (setter) _ped_CHSGeometry_set,
-                  "CHSGeometry cylinders", "cylinders"},
+                  "The number of cylinders.", "cylinders"},
     {"heads", (getter) _ped_CHSGeometry_get,
               (setter) _ped_CHSGeometry_set,
-              "CHSGeometry heads", "heads"},
+              "The number of heads", "heads"},
     {"sectors", (getter) _ped_CHSGeometry_get,
                 (setter) _ped_CHSGeometry_set,
-                "CHSGeometry sectors", "sectors"},
+                "The number of sectors", "sectors"},
     {NULL}  /* Sentinel */
 };
+
+PyDoc_STRVAR(_ped_CHSGeometry_doc,
+"A _ped.CHSGeometry object describes a disk using the older CHS style\n"
+"of defining disk geometry.  CHS stands for cylinders-heads-sectors.\n\n"
+"The _ped.CHSGeometry objects are created automatically when devices are\n"
+"probed by libparted.  They are used for reference purposes to get the\n"
+"number of cylinders, heads, or sectors on a disk.  They cannot be used\n"
+"to change the CHS values on a device.");
 
 static PyTypeObject _ped_CHSGeometry_Type_obj = {
     PyObject_HEAD_INIT(&PyType_Type)
@@ -105,7 +200,7 @@ static PyTypeObject _ped_CHSGeometry_Type_obj = {
  /* .tp_setattro = XXX */
  /* .tp_as_buffer = XXX */
     .tp_flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,
-    .tp_doc = "PedCHSGeometry objects",
+    .tp_doc = _ped_CHSGeometry_doc,
  /* .tp_traverse = XXX */
  /* .tp_clear = XXX */
  /* .tp_richcompare = XXX */
@@ -157,30 +252,42 @@ typedef struct {
 } _ped_Device;
 
 static PyMemberDef _ped_Device_members[] = {
-    {"hw_geom", T_OBJECT, offsetof(_ped_Device, hw_geom), 0, NULL},
-    {"bios_geom", T_OBJECT, offsetof(_ped_Device, bios_geom), 0, NULL},
+    {"hw_geom", T_OBJECT, offsetof(_ped_Device, hw_geom), 0,
+                "The CHSGeometry of the Device as reported by the hardware."},
+    {"bios_geom", T_OBJECT, offsetof(_ped_Device, bios_geom), 0,
+                  "The CHSGeometry of the Device as reported by the BIOS."},
     {NULL}
 };
 
 static PyMethodDef _ped_Device_methods[] = {
-    {"get_next", (PyCFunction) py_ped_device_get_next, METH_VARARGS, NULL},
-    {"is_busy", (PyCFunction) py_ped_device_is_busy, METH_VARARGS, NULL},
-    {"open", (PyCFunction) py_ped_device_open, METH_VARARGS, NULL},
-    {"close", (PyCFunction) py_ped_device_close, METH_VARARGS, NULL},
-    {"destroy", (PyCFunction) py_ped_device_destroy, METH_VARARGS, NULL},
+    {"get_next", (PyCFunction) py_ped_device_get_next, METH_VARARGS,
+                 device_get_next_doc},
+    {"is_busy", (PyCFunction) py_ped_device_is_busy, METH_VARARGS,
+                device_is_busy_doc},
+    {"open", (PyCFunction) py_ped_device_open, METH_VARARGS,
+             device_open_doc},
+    {"close", (PyCFunction) py_ped_device_close, METH_VARARGS,
+              device_close_doc},
+    {"destroy", (PyCFunction) py_ped_device_destroy, METH_VARARGS,
+                device_destroy_doc},
     {"cache_remove", (PyCFunction) py_ped_device_cache_remove,
-                     METH_VARARGS, NULL},
+                     METH_VARARGS, device_cache_remove_doc},
     {"begin_external_access", (PyCFunction) py_ped_device_begin_external_access,
-                              METH_VARARGS, NULL},
+                              METH_VARARGS, device_begin_external_access_doc},
     {"end_external_access", (PyCFunction) py_ped_device_end_external_access,
-                            METH_VARARGS, NULL},
-    {"read", (PyCFunction) py_ped_device_read, METH_VARARGS, NULL},
-    {"write", (PyCFunction) py_ped_device_write, METH_VARARGS, NULL},
-    {"sync", (PyCFunction) py_ped_device_sync, METH_VARARGS, NULL},
-    {"sync_fast", (PyCFunction) py_ped_device_sync_fast, METH_VARARGS, NULL},
-    {"check", (PyCFunction) py_ped_device_check, METH_VARARGS, NULL},
+                            METH_VARARGS, device_end_external_access_doc},
+    {"read", (PyCFunction) py_ped_device_read, METH_VARARGS,
+             device_read_doc},
+    {"write", (PyCFunction) py_ped_device_write, METH_VARARGS,
+              device_write_doc},
+    {"sync", (PyCFunction) py_ped_device_sync, METH_VARARGS,
+             device_sync_doc},
+    {"sync_fast", (PyCFunction) py_ped_device_sync_fast, METH_VARARGS,
+                  device_sync_fast_doc},
+    {"check", (PyCFunction) py_ped_device_check, METH_VARARGS,
+              device_check_doc},
     {"get_constraint", (PyCFunction) py_ped_device_get_constraint,
-                       METH_VARARGS, NULL},
+                       METH_VARARGS, device_get_constraint_doc},
     {NULL}
 };
 
@@ -219,6 +326,12 @@ static PyGetSetDef _ped_Device_getset[] = {
     {NULL}  /* Sentinel */
 };
 
+PyDoc_STRVAR(_ped_Device_doc,
+"A _ped.Device object describes a block device accessible via the\n"
+"operating system.  On Linux, an example block device is /dev/sda\n\n"
+"It is important to note that _ped.Device objects describe entire\n"
+"block devices and not just partitions.");
+
 static PyTypeObject _ped_Device_Type_obj = {
     PyObject_HEAD_INIT(&PyType_Type)
     .tp_name = "_ped.Device",
@@ -240,7 +353,7 @@ static PyTypeObject _ped_Device_Type_obj = {
  /* .tp_setattro = XXX */
  /* .tp_as_buffer = XXX */
     .tp_flags = Py_TPFLAGS_HAVE_CLASS | Py_TPFLAGS_BASETYPE,
-    .tp_doc = "PedDevice objects",
+    .tp_doc = _ped_Device_doc,
  /* .tp_traverse = XXX */
  /* .tp_clear = XXX */
  /* .tp_richcompare = XXX */
