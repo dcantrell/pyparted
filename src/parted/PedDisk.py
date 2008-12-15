a6edbd462a93a995d4574cd3e97700ead19b18cd
Chris Lumens <clumens@redhat.com>
2008-12-15 16:55:00 -0500

    Add a more complete PedDisk object, thought it's not yet accessible.
    parted.PedDisk gets used, but there's also parted.PedDisk.new which we still
    need to make visible so anaconda can call it.  I haven't reconciled that
    problem yet, but for now here's the full parted.PedDisk class.

--- src/parted/PedDisk.py
+++ src/parted/PedDisk.py
@@ -1,8 +1,8 @@
 #
-# PedDevice.py
+# PedDisk.py
 # Python bindings for libparted (built on top of the _ped Python module).
 #
-# Copyright (C) 2007  Red Hat, Inc.
+# Copyright (C) 2007, 2008 Red Hat, Inc.
 # All rights reserved.
 #
 # This copyrighted material is made available to anyone wishing to use,
@@ -28,59 +28,118 @@ import warnings
 __depstr = "%s is deprecated and will be removed."
 
 class PedDisk(object):
-    def __init__(self, path):
-        warnings.warn("PedDevice class is deprecated by Device", DeprecationWarning, stacklevel=2)
+    def __init__(self, type):
+        warnings.warn("PedDisk class is deprecated by Disk", DeprecationWarning, stacklevel=2)
+        self.__disk = _ped.Disk(type)
 
     def __setx(self, value):
         raise AttributeError, "attribute is read-only"
 
-#    length = property(lambda self: self.__device.length, __setx)
-#    model = property(lambda self: self.__device.model, __setx)
-#    path = property(lambda self: self.__device.path, __setx)
-#    sector_size = property(lambda self: self.__device.sector_size, __setx)
-#    type = property(lambda self: self.__device.type, __setx)
-#    heads = property(lambda self: self.__device.heads, __setx)
-#    sectors = property(lambda self: self.__device.sectors, __setx)
-#    cylinders = property(lambda self: self.__device.cylinders, __setx)
+    dev = property(lambda self: self.__device.dev, __setx)
+    type = property(lambda self: self.__device.type, __setx)
+    max_primary_partition_count = property(lambda self: self.__device.get_max_primary_partition_count(), __setx)
+    extended_partition = property(lambda self: self.__device.extended_partition(), __setx)
 
-    def new(self):
+    def next_partition(self, parttype):
         warnings.warn("PedDisk class is deprecated by Disk", DeprecationWarning, stacklevel=2)
 
-    def check_feature(self):
+        try:
+            return self.__disk.next_partition(parttype)
+        except TypeError:
+            raise
+        except Exception, e:
+            raise parted.error(e.message)
+
+    def delete_partition(self, parttype):
         warnings.warn("PedDisk class is deprecated by Disk", DeprecationWarning, stacklevel=2)
 
-    def next_partition(self):
-        warnings.warn("PedDisk class is deprecated by Disk", DeprecationWarning, stacklevel=2)
-
-    def delete_partition(self):
-        warnings.warn("PedDisk class is deprecated by Disk", DeprecationWarning, stacklevel=2)
+        try:
+            return self.__disk.delete_partition(parttype)
+        except TypeError:
+            raise
+        except Exception, e:
+            raise parted.error(e.message)
 
     def delete_all(self):
         warnings.warn("PedDisk class is deprecated by Disk", DeprecationWarning, stacklevel=2)
 
-    def add(self):
+        try:
+            return self.__disk.delete_all()
+        except Exception, e:
+            raise parted.error(e.message)
+
+    def add(self, part, constraint):
         warnings.warn("PedDisk class is deprecated by Disk", DeprecationWarning, stacklevel=2)
 
-    def get_partition(self):
+        try:
+            return self.__disk.add(part, constraint)
+        except TypeError:
+            raise
+        except Exception, e:
+            raise parted.error(e.message)
+
+    def get_partition(self, num):
         warnings.warn("PedDisk class is deprecated by Disk", DeprecationWarning, stacklevel=2)
 
-    def get_partition_by_sector(self):
+        try:
+            return self.__disk.get_partition(part, num)
+        except Exception, e:
+            raise parted.error(e.message)
+
+    def get_partition_by_sector(self, sector):
         warnings.warn("PedDisk class is deprecated by Disk", DeprecationWarning, stacklevel=2)
 
-    def partition_new(self):
+        try:
+            return self.__disk.get_partition_by_sector(part, sector)
+        except Exception, e:
+            raise parted.error(e.message)
+
+    def partition_new(self, type, fs, start, end):
         warnings.warn("PedDisk class is deprecated by Disk", DeprecationWarning, stacklevel=2)
+
+        try:
+            raise _ped.Partition(self.__device, type, fs, start, end)
+        except TypeError:
+            raise
+        except Exception, e:
+            raise parted.error(e.message)
 
     def minimum_extended_partition(self):
         warnings.warn("PedDisk class is deprecated by Disk", DeprecationWarning, stacklevel=2)
 
-    def maximize_partition(self):
+        try:
+            return self.__disk.minimize_extended_partition()
+        except Exception, e:
+            raise parted.error(e.message)
+
+    def maximize_partition(self, part, constraint):
         warnings.warn("PedDisk class is deprecated by Disk", DeprecationWarning, stacklevel=2)
+
+        try:
+            return self.__disk.maximize_partition(part, constraint)
+        except Exception, e:
+            raise parted.error(e.message)
 
     def get_primary_partition_count(self):
         warnings.warn("PedDisk class is deprecated by Disk", DeprecationWarning, stacklevel=2)
 
+        try:
+            return self.__disk.get_primary_partition_count()
+        except Exception, e:
+            raise parted.error(e.message)
+
     def get_last_partition_num(self):
         warnings.warn("PedDisk class is deprecated by Disk", DeprecationWarning, stacklevel=2)
 
+        try:
+            return self.__disk.get_last_partition_num()
+        except Exception, e:
+            raise parted.error(e.message)
+
     def commit(self):
         warnings.warn("PedDisk class is deprecated by Disk", DeprecationWarning, stacklevel=2)
+
+        try:
+            return self.__disk.commit()
+        except Exception, e:
+            raise parted.error(e.message)
