8c7d6b1c82b5a8e5eaf44052d3687f3c36c626c3
Chris Lumens <clumens@redhat.com>
2009-01-13 16:51:02 -0500

    Add stub test case files for all the other _ped classes.

--- tests/_ped/test_disk.py
+++ tests/_ped/test_disk.py
@@ -1,14 +1,13 @@
+#!/usr/bin/python
 #
-# Makefile.am for pyparted
-#
-# Copyright (C) 2008  Red Hat, Inc.
+# Copyright (C) 2009  Red Hat, Inc.
 #
 # This copyrighted material is made available to anyone wishing to use,
 # modify, copy, or redistribute it subject to the terms and conditions of
 # the GNU General Public License v.2, or (at your option) any later version.
 # This program is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY expressed or implied, including the implied warranties of
-# MERCHANTABILITY or FITNESS FOR A * PARTICULAR PURPOSE.  See the GNU General
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
 # Public License for more details.  You should have received a copy of the
 # GNU General Public License along with this program; if not, write to the
 # Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
@@ -19,18 +18,18 @@
 #
 # Red Hat Author(s): Chris Lumens <clumens@redhat.com>
 #
+import _ped
+import unittest
 
-EXTRA_DIST = baseclass.py $(TESTS)
-MAINTAINERCLEANFILES = Makefile.in
+# One class per method, multiple tests per class.  For these simple methods,
+# that seems like good organization.  More complicated methods may require
+# multiple classes and their own test suite.
 
-TESTS_ENVIRONMENT = PYTHONPATH=$(top_builddir)/src/.libs
 
-TESTS = test_ped.py \
-		  test_alignment.py \
-		  test_chsgeometry.py \
-		  test_constraint.py \
-		  test_device.py \
-		  test_disk.py \
-		  test_filesystem.py \
-		  test_geometry.py \
-		  test_partition.py
+# And then a suite to hold all the test cases for this module.
+def suite():
+    suite = unittest.TestSuite()
+    return suite
+
+s = suite()
+unittest.TextTestRunner(verbosity=2).run(s)
