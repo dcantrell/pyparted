b6370d3e97b534aecc18fc9e379485172e2714da
David Cantrell <dcantrell@redhat.com>
2009-01-28 10:31:12 -1000

    Allow start and end sectors for parted.Geometry init.
    Allow users to specify start and end sectors and have __init__
    compute the length of the geometry.

--- src/parted/geometry.py
+++ src/parted/geometry.py
@@ -41,8 +41,12 @@ class Geometry(object):
             self.__geometry = PedGeometry
         elif not end:
             self.__geometry = _ped.Geometry(device.getPedDevice(), start, length)
-        else:
+        elif not length and (end > start):
+            self.__geometry = _ped.Geometry(device.getPedDevice(), start, (end - start), end)
+        elif start and length and end and (end > start):
             self.__geometry = _ped.Geometry(device.getPedDevice(), start, length, end=end)
+        else:
+            raise _ped.PartedException, "must specify PedGeometry or (device, start, length) or (device, start, end) or (device, start, length, end)"
 
     def __readOnly(self, property):
         raise parted.ReadOnlyProperty, property
