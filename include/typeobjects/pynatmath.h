68a4e10e76efc69b4f37dd52af977ff79b719d43
David Cantrell <dcantrell@redhat.com>
2009-04-06 16:17:34 -1000

    Add tp_compare and tp_richcompare for Alignment.

--- include/typeobjects/pynatmath.h
+++ include/typeobjects/pynatmath.h
@@ -2,7 +2,7 @@
  * pynatmath.h
  * pyparted type objects for pynatmath.c
  *
- * Copyright (C) 2007, 2008  Red Hat, Inc.
+ * Copyright (C) 2007, 2008, 2009  Red Hat, Inc.
  *
  * This copyrighted material is made available to anyone wishing to use,
  * modify, copy, or redistribute it subject to the terms and conditions of
@@ -67,7 +67,7 @@ PyTypeObject _ped_Alignment_Type_obj = {
     .tp_dealloc = (destructor) _ped_Alignment_dealloc,
  /* .tp_getattr = XXX */
  /* .tp_setattr = XXX */
- /* .tp_compare = XXX */
+    .tp_compare = (cmpfunc) _ped_Alignment_compare,
  /* .tp_repr = XXX */
  /* .tp_as_number = XXX */
  /* .tp_as_sequence = XXX */
@@ -79,11 +79,11 @@ PyTypeObject _ped_Alignment_Type_obj = {
     .tp_setattro = PyObject_GenericSetAttr,
  /* .tp_as_buffer = XXX */
     .tp_flags = Py_TPFLAGS_HAVE_CLASS | Py_TPFLAGS_BASETYPE |
-                Py_TPFLAGS_HAVE_GC,
+                Py_TPFLAGS_HAVE_GC | Py_TPFLAGS_HAVE_RICHCOMPARE,
     .tp_doc = _ped_Alignment_doc,
     .tp_traverse = (traverseproc) _ped_Alignment_traverse,
     .tp_clear = (inquiry) _ped_Alignment_clear,
- /* .tp_richcompare = XXX */
+    .tp_richcompare = (richcmpfunc) _ped_Alignment_richcompare,
  /* .tp_weaklistoffset = XXX */
  /* .tp_iter = XXX */
  /* .tp_iternext = XXX */
