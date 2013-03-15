c34a4f39c12bcfc82e035325bb9d25416eee00d2
Gabriel <g2p.code@gmail.com>
2013-03-15 14:43:03 +0100

    Fix comparisons. Those used to crash the testsuite.

--- src/pynatmath.c
+++ src/pynatmath.c
@@ -64,18 +64,11 @@ int _ped_Alignment_compare(_ped_Alignment *self, PyObject *obj) {
 }
 
 PyObject *_ped_Alignment_richcompare(_ped_Alignment *a, PyObject *b, int op) {
-    if (op == Py_EQ) {
-        if ((_ped_Alignment_Type_obj.tp_richcompare((PyObject *) a, b, Py_EQ))) {
-            Py_RETURN_TRUE;
-        } else {
-            Py_RETURN_FALSE;
-        }
-    } else if (op == Py_NE) {
-        if (!(_ped_Alignment_Type_obj.tp_richcompare((PyObject *) a, b, Py_EQ))) {
-            Py_RETURN_TRUE;
-        } else {
-            Py_RETURN_FALSE;
-        }
+    if (op == Py_EQ || op == Py_NE) {
+        int rv = _ped_Alignment_compare(a, b);
+        if (PyErr_Occurred())
+            return NULL;
+        return PyBool_FromLong(op == Py_EQ ? rv == 0 : rv != 0);
     } else if ((op == Py_LT) || (op == Py_LE) ||
                (op == Py_GT) || (op == Py_GE)) {
         PyErr_SetString(PyExc_TypeError, "comparison operator not supported for _ped.Alignment");
