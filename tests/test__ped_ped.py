c14b5027342930c412911b3ca3555b7b6ef5f49a
Gabriel <g2p.code@gmail.com>
2013-03-15 12:51:09 +0100

    Skip a test that relies on being root.

--- tests/test__ped_ped.py
+++ tests/test__ped_ped.py
@@ -193,6 +193,7 @@ class DeviceGetTestCase(RequiresDevice):
         self.assertRaises(_ped.IOException, _ped.device_get, "")
         self.assertRaises(_ped.DeviceException, _ped.device_get, None)
 
+@unittest.skip('Requires root')
 class DeviceGetNextTestCase(unittest.TestCase, BuildList):
     def runTest(self):
         # Make sure there are some devices in the system first and then
