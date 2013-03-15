cb343ba28352bbce597a281f68481bbd82ccd6b2
Gabriel <g2p.code@gmail.com>
2013-03-15 23:30:49 +0100

    Version bump.

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ from distutils.errors import LinkError
 from distutils.core import setup
 from distutils.core import Extension
 
-pyparted_version = '3.9'
+pyparted_version = '3.10a1'
 python_version = sys.version_info
 
 need_libparted_version = '2.3'
