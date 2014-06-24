f65e94b92764c4e28ab97013c8f6f7fb23f1801c
David Cantrell <dcantrell@redhat.com>
2014-06-24 11:13:25 -0400

    Release version 3.9.4.

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from distutils.errors import LinkError
 from distutils.core import setup
 from distutils.core import Extension
 
-pyparted_version = '3.9.3'
+pyparted_version = '3.9.4'
 python_version = platform.python_version_tuple()
 
 need_libparted_version = '3.1'
