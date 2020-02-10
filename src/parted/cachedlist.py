dcantrell/pyparted

commit e118f7913581b5b1fdd88dda73cd77b41e93ffad

'from collections' -> 'from collections.abc' (BZ#1772060)

src/parted/cachedlist.py
@@ -1,7 +1,7 @@
 #
 # Python bindings for libparted (built on top of the _ped Python module).
 #
-# Copyright (C) 2009 Red Hat, Inc.
+# Copyright (C) 2009-2019 Red Hat, Inc.
 #
 # This copyrighted material is made available to anyone wishing to use,
 # modify, copy, or redistribute it subject to the terms and conditions of
@@ -18,9 +18,10 @@
 # Red Hat, Inc.
 #
 # Red Hat Author(s): Chris Lumens <clumens@redhat.com>
+#                    David Cantrell <dcantrell@redhat.com>
 #
 
-from collections import Sequence
+from collections.abc import Sequence
 
 class CachedList(Sequence):
     """CachedList()

commit 447a7c5bc0b8038f2cf9865271acb08655c55464

Don't intentionally prohibit Python 2 usage (#67)
Commit e118f79 broke any attempt at usage in Python 2.  While work
currently happens on Python 3, I am not going out of my way to render
pyparted non-functional on Python 2.

This commit just performs the right Sequence import depending on the
Python major and minor version.

src/parted/cachedlist.py
@@ -1,7 +1,7 @@
 #
 # Python bindings for libparted (built on top of the _ped Python module).
 #
-# Copyright (C) 2009-2019 Red Hat, Inc.
+# Copyright (C) 2009-2020 Red Hat, Inc.
 #
 # This copyrighted material is made available to anyone wishing to use,
 # modify, copy, or redistribute it subject to the terms and conditions of
@@ -21,7 +21,12 @@
 #                    David Cantrell <dcantrell@redhat.com>
 #
 
-from collections.abc import Sequence
+import sys
+
+if sys.version_info.major >= 3 and sys.version_info.minor >= 3:
+    from collections.abc import Sequence
+else:
+    from collections import Sequence
 
 class CachedList(Sequence):
     """CachedList()